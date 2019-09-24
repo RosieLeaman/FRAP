e8e6b665db46fc31a3cf2184814f128709451a96
RosieLeaman <rosie.leaman@gmail.com>
2019-09-24 23:34:12 +0100

    cleaned up some of the comments

--- FRAP_folder.m
+++ FRAP_folder.m
@@ -1,7 +1,7 @@
-% when provided with a folder reads in all the files necessary for FRAP
+% When provided with a folder reads in all the files necessary for FRAP
 % all the files in this folder should be of the form:
-% NAME_1.ome.tif;NAME_1minpost_1.ome.tif...
-% NAME_2.ome.tif;NAME_1minpost_2.ome.tif...
+% NAME_1.lsm;NAME_1minpost_1.lsm...
+% NAME_2.lsm;NAME_1minpost_2.lsm...
 % the important part is that all the images that are part of the same set
 % must end in the same number
 % and each set must have THE SAME NUMBER OF IMAGES
@@ -16,31 +16,20 @@ folder = uigetdir('','Choose image folder');
 % these are microscopy files
 fileList = dir(fullfile(folder,'*.lsm'));
 
-% % find out how many FRAP sets there are
-% 
-% FRAPsets = input('How many sets of images are there?\n');
-% 
-% % find out how many images per sets there are
-% 
-% numImages = input('How many time points are there (including the prebleach image)?\n');
-
+% find out how many FRAP sets and images per set there are
 [FRAPsets,numImages] = getNumbers();
 
 % create storage for the signals and times used
-
 signals = zeros(FRAPsets,numImages);
 timestamps = zeros(FRAPsets,numImages);
 
 % determine how much information we want to display
-
 [plotRecoveryYes,averageRecoveryYes,driftYes,plotMasksYes,detailYes] = checkboxes();
 
 % plotMasksYes = 1; % plots a summary of masks for bleached and non-bleached regions
 % plotRecoveryYes = 1; % plots a recovery curve for each image set
 % detailYes = 0; % plots almost every single mask used (for debugging only)
-% driftYes = 1;
-
-% this will be turned into a checkbox at some point for input 
+% driftYes = 1; % accounts for drift in the images
 
 for i=1:FRAPsets
     disp(['Working on set ',num2str(i)])
@@ -51,28 +40,28 @@ for i=1:FRAPsets
     imageCount = 0;
     for j=1:length(fileList)
         fileEnd = [num2str(i),'.lsm'];
+        
+        % there is a slight awkwardness when comparing file names as 10 has
+        % more digits than 1, so a different comparison is used for < 10
+        % and >= 10
         if i < 10
-            %fileList(j).name(end-5)
-            %fileList(j).name(end-4:end)
-            %strcmp(fileList(j).name(end-5),'-')
-            %strcmp(fileList(j).name(end-4:end),fileEnd)
             if strcmp(fileList(j).name(end-4:end),fileEnd)  && strcmp(fileList(j).name(end-5),'-')
                 % we've found a right file as the number in the filename
                 % matches the FRAP set number
                 disp(['Found a file for this set ',fileList(j).name])
                 disp('Reading in file\n')
 
-                T = tiffread([folder,'/',fileList(j).name]);
+                T = tiffread([folder,'/',fileList(j).name]); % read file
 
                 % for each frame in this image stack add it to the images and
                 % its timestamp too
-
                 for k=1:length(T)
                     imageCount = imageCount + 1;
                     images{imageCount} = T(k).data;
                     times(imageCount) = T(k).lsm.TimeStamp(k);
                 end
             end
+            
         elseif strcmp(fileList(j).name(end-5:end),fileEnd)
             % we've found a right file as the number in the filename
             % matches the FRAP set number
@@ -83,7 +72,6 @@ for i=1:FRAPsets
 
             % for each frame in this image stack add it to the images and
             % its timestamp too
-
             for k=1:length(T)
                 imageCount = imageCount + 1;
                 images{imageCount} = T(k).data;
@@ -94,58 +82,42 @@ for i=1:FRAPsets
     % now we have all the files for this FRAP set
     % we then need to arrange the images in time order
     
-    % use this snazzy bit of matlab code from the internet to sort the
-    % image vector according to the sorted time vector
-    % https://uk.mathworks.com/matlabcentral/newsreader/view_thread/83722
-    
+    % use this bit of matlab code from https://uk.mathworks.com/matlabcentral/newsreader/view_thread/83722
+    % to sort the image vector according to the sorted time vector
     [sortedTimes,ind] = sort(times);
     sortedImages = images(ind);
 
     % now that the images are sorted we can do the FRAP!
-
     signals(i,:) = FRAP_main(sortedImages,sortedTimes,plotMasksYes,plotRecoveryYes,detailYes,driftYes);
     
-    % make the times relative to the first time
-    
+    % make the times relative to the first time, as the times are currently
+    % system times the images were taken
     relTimes = sortedTimes - sortedTimes(1);
     
-    % save the timestamps too
-    
+    % save the relative timestamps too
     timestamps(i,:) = relTimes;
     
     if(plotRecoveryYes)
+        % save the individual recovery curve
         filename = [folder,'/recovery-',num2str(i),'.tif'];
         print('-dtiff', '-r400', filename);
     end
 end
 
-% save the data to a text file comma-separated
-
+% save the data to a text file, comma-separated.
 dlmwrite([folder,'/times.txt'],timestamps)
 dlmwrite([folder,'/curves.txt'],signals)
 
 if(averageRecoveryYes)
-    % calculate the average recovery curve and plot it
-
-    % later images are often taken at not quite the same time
-    % choose the smallest of these to be our 'precise times'
-    % we pick the smallest as then that timepoint is within all the measured
-    % time points across all curves, so no extrapolation only interpolation
-
-    %preciseTimes = [min(times(:,1)),min(times(:,2)),min(times(:,3)),min(times(:,4)),min(times(:,5)),min(times(:,6)),min(times(:,7)),min(times(:,8))];
-
     % calculate the average recovery curve
     [avg,time] = calcAverage(signals,timestamps);
-
-    % find the value on the average curve at that timepoint
-    %avgSignals = interp1(time,avg,preciseTimes);
-
+    
+    % plot the curve
     figure;plot(time,avg,'Linewidth',3);grid on;grid minor;set(gca,'FontSize',20);hold on
     %errorbar(preciseTimes(1:6),avgSignals(1:6),stdev(1:6),'kx','Linewidth',2); 
     xlabel('Time postbleach (s)');ylabel('Normalised intensity');
     text(250,0.95,['n=',num2str(FRAPsets)],'FontSize',20,'Color','black');
     
     filename = [folder,'/recovery-average.tif'];
-    print('-dtiff', '-r400', strcat(filename));
-    
+    print('-dtiff', '-r400', strcat(filename)); % save to file
 end
--- calcAverage.m
+++ calcAverage.m
@@ -11,21 +11,17 @@ n = size(signals,1);
 % determine the maximum timepoint
 % this is the minimum of the final column of T (do not want to
 % extrapolate!)
-
 m = min(times(:,end));
 
 % create the 'query points' where we shall interpolate each to find their
 % value
-
 numQueries = 1000; % number of query points to be used
 xq = linspace(1,m,numQueries);
 
 % create the matrix to store the results
-
 interpolated = zeros(n,numQueries+1);
 
 % go through each signal and interpolate it at our query points
-
 for i=1:n
     interpolated(i,2:end) = interp1(times(i,:),signals(i,:),xq);
     interpolated(i,1) = 1;
@@ -37,7 +33,6 @@ avgSignals = zeros(1,numQueries);
 errors = zeros(1,numQueries);
 
 % average the signals and find the errors
-
 for i=1:numQueries
     avgSignals(i) = mean(interpolated(:,i));
     errors(i) = std(interpolated(:,i));
